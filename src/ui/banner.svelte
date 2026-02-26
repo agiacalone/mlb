3809442827cdf3a6e3cd8aaf9a317214e2d4fd29
Mitchell Christ <mitchell@nuotsu.dev>
2026-02-25 23:45:08 -0800

    CSS Design Awards!! 🤩

--- src/ui/banner.svelte
+++ src/ui/banner.svelte
@@ -1,35 +1,34 @@
 <script lang="ts">
-	import { colorSchemeStore } from '$ui/store.svelte'
+	const IMAGES = ['cssda-special-kudos', 'cssda-best-ui', 'cssda-best-ux', 'cssda-best-inn']
 </script>
 
 <aside
-	class="group/banner relative flex items-center gap-lh p-ch text-center leading-tight text-balance before:bg-accent/25 dark:text-accent"
+	class="group/banner relative isolate mt-[1.5lh] flex flex-wrap items-center justify-center gap-x-lh gap-y-ch p-ch text-center leading-tight text-balance text-yellow-700 before:bg-amber-300/25 dark:text-amber-300"
 >
-	<p class="grow">
-		<em>CSS Design Awards</em> Nominee —
+	<p>
+		<em>CSS Design Awards</em> —
 		<a
 			class="decoration-dashed group-hover/banner:underline"
 			href="https://www.cssdesignawards.com/sites/mlb-live-scorebug/48948/"
 		>
-			Vote now!
-			<span class="absolute inset-0"></span>
+			<strong>Special Kudos + Best UI + Best UX + Best Innovation</strong>
+			<span class="absolute inset-0 z-10"></span>
 		</a>
 	</p>
 
-	<picture class="shrink-0">
-		<source
-			srcset="https://www.cssdesignawards.com/images/2024/monogram/cssda-nominee-white-vote.png"
-			media={colorSchemeStore.colorScheme === 'dark' ? undefined : '()'}
-		/>
-		<img
-			class="size-[3lh]"
-			src="https://www.cssdesignawards.com/images/2024/monogram/cssda-nominee-dark-vote.png"
-			alt=""
-			width="220"
-			height="220"
-			draggable="false"
-		/>
-	</picture>
+	<figure class="flex items-center *:not-first:-ml-ch">
+		{#each IMAGES as image, i}
+			<img
+				class="size-[3lh]"
+				style:z-index={IMAGES.length - i}
+				src="/awards/{image}.png"
+				alt=""
+				width="220"
+				height="220"
+				draggable="false"
+			/>
+		{/each}
+	</figure>
 </aside>
 
 <style>
