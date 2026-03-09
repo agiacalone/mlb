f346c6f24a3c14b77d1de33b0ac517a1a483d334
Mitchell Christ <mitchell@nuotsu.dev>
2026-03-08 18:59:10 -0700

    update wbc banner

--- src/ui/banner/wbc.svelte
+++ src/ui/banner/wbc.svelte
@@ -4,16 +4,27 @@
 </script>
 
 <aside
-	class="items-between relative flex grid-cols-[1fr_auto_1fr] items-center justify-between gap-ch bg-[#006e3c]/25 px-ch text-center font-bold before:m-auto md:grid dark:bg-[#006e3c]/80 not-dark:text-[#006e3c]"
+	class="items-between relative flex grid-cols-[1fr_auto_1fr] items-center justify-between gap-ch bg-[#006e3c]/25 px-ch py-[.25ch] text-center before:m-auto md:grid dark:bg-[#006e3c]/80 not-dark:text-[#006e3c]"
 >
-	<a
-		class="leading-tight text-balance decoration-dashed hover:underline"
-		href="/schedule/week/{formatDate(getToday(), { locale: 'en-CA' })}?sportId=51"
-	>
-		World Baseball Classic Schedule
-		<ChevronRightIcon class="inline-block size-lh align-top max-sm:hidden" />
-		<span class="absolute inset-0"></span>
-	</a>
+	<div class="flex flex-wrap items-center justify-center gap-x-ch gap-y-[.25ch]">
+		<span>World Baseball Classic</span>
+
+		<a
+			class="leading-tight font-bold text-balance decoration-dashed hover:underline"
+			href="/schedule/week/{formatDate(getToday(), { locale: 'en-CA' })}?sportId=51"
+		>
+			Schedule
+			<ChevronRightIcon class="inline-block size-lh align-top" />
+		</a>
+
+		<a
+			class="leading-tight font-bold text-balance decoration-dashed hover:underline"
+			href="/standings/2026?sportId=51&gameType=P"
+		>
+			Standings
+			<ChevronRightIcon class="inline-block size-lh align-top" />
+		</a>
+	</div>
 
 	<img
 		class="-my-ch ml-auto"
