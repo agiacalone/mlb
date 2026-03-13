5721bc52833736a265b99172ed307403427572dc
Mitchell Christ <mitchell@nuotsu.dev>
2026-03-12 18:38:16 -0700

    update error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
 	import { page } from '$app/state'
-	import Empty from '$ui/empty.svelte'
 </script>
 
-<section class="grid h-full place-content-center">
-	<Empty>
-		<p class="tabular-nums">{page.status}</p>
+<section class="grid h-full place-content-center p-ch text-center">
+	<div
+		class="m-auto flex flex-col place-content-center gap-ch border border-dashed border-stroke p-lh"
+	>
+		<span class="text-current/40">{':('}</span>
+
+		<p class="text-current/40 tabular-nums">{page.status}</p>
 
 		{#if page.error?.message}
 			<h1>{page.error?.message}</h1>
@@ -13,12 +16,14 @@
 			<h1>An error occurred.</h1>
 		{/if}
 
-		<div class="flex items-center justify-center gap-ch text-foreground">
+		<hr class="border-dashed border-stroke" />
+
+		<div class="flex items-center justify-center gap-lh text-foreground">
 			<a class="link" href="/">Return home</a>
 
 			{#if page.status !== 404}
 				<a class="link" href={page.url.toString()}>Try again</a>
 			{/if}
 		</div>
-	</Empty>
+	</div>
 </section>
